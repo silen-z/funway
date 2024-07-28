2d342245fbdaa528c9cd7e164ca13d70b992a93d
Pavel Hrách <pavel@hrach.email>
2024-07-28 23:59:53 +0200

    implement HandlerChain as linked list

--- packages/fiveway/src/handler.ts
+++ packages/fiveway/src/handler.ts
@@ -17,11 +17,6 @@ export type NavigationHandler = (
   next: HandlerNext
 ) => NodeId | null;
 
-export type HandlerChain = NavigationHandler & {
-  prepend(another: NavigationHandler): HandlerChain;
-  append(another: NavigationHandler): HandlerChain;
-};
-
 export function runHandler(
   tree: NavigationTree,
   id: NodeId,
@@ -39,54 +34,63 @@ export function runHandler(
   return node.handler(node, action, next);
 }
 
+export type HandlerChain = NavigationHandler & {
+  prepend(another: NavigationHandler): HandlerChain;
+};
+
 /**
  * Take handlers and combines them into one so the next handler function
  * automatically passes action to the next handler
  *
- * implementation inspired by:  https://deno.land/x/oak@v16.1.0/middleware.ts?source=#L60
- *
  * @param handlers handlers that will be called in order first to last
  * @returns handler that will pipe navigation actions through via the next function
  */
-export function chainHandlers(
-  ...handlers: NavigationHandler[]
-): HandlerChain {
-  const stack = handlers.slice();
+export function chainHandlers(...handlers: NavigationHandler[]): HandlerChain {
+  const chain = chainFromArray(handlers);
+  return chainedHandler(chain);
+}
 
-  const composedHandler = (
+function chainedHandler(chain: ChainLink | null) {
+  const handler = (
     node: NavigationNode,
     action: NavigationAction,
     next: HandlerNext
   ): NodeId | null => {
-    let index = -1;
-    function dispatch(i: number): NodeId | null {
-      if (i <= index) {
-        throw new Error("next() called multiple times.");
+    const dispatch = (
+      link: ChainLink | null,
+      id?: NodeId,
+      newAction?: NavigationAction
+    ): NodeId | null => {
+      if (id != null) {
+        return next(id, newAction ?? action);
       }
-      index = i;
 
-      const fn = stack[i];
-      if (fn == null) {
+      if (link == null) {
         return next();
       }
 
-      return fn(node, action, (id?: NodeId, newAction?: NavigationAction) => {
-        if (id != null) {
-          return next(id, newAction ?? action);
-        }
+      return link.handler(node, action, dispatch.bind(null, link.next));
+    };
 
-        return dispatch(i + 1);
-      });
-    }
-
-    return dispatch(0);
+    return dispatch(chain);
   };
 
-  composedHandler.append = (handler: NavigationHandler) =>
-    chainHandlers(...stack, handler);
+  handler.prepend = (handler: NavigationHandler) =>
+    chainedHandler({ handler, next: chain });
 
-  composedHandler.prepend = (handler: NavigationHandler) =>
-    chainHandlers(handler, ...stack);
+  return handler;
+}
 
-  return composedHandler;
+type ChainLink = {
+  handler: NavigationHandler;
+  next: ChainLink | null;
+};
+
+function chainFromArray(arr: NavigationHandler[]): ChainLink | null {
+  const head = arr.reduceRight<ChainLink | null>(
+    (prev, a) => ({ handler: a, next: prev }),
+    null
+  );
+
+  return head;
 }
