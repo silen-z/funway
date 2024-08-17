041c6b33c0942a73c62d70c10b6574dc0b06c01f
Pavel Hrách <pavel@hrach.email>
2024-08-17 17:06:48 +0200

    add a way to extend NavigationAction

--- packages/fiveway/src/navigation.ts
+++ packages/fiveway/src/navigation.ts
@@ -2,10 +2,28 @@ import { type NavigationTree, focusNode } from "./tree.js";
 import { runHandler } from "./handler.js";
 
 export type NavigationDirection = "up" | "down" | "left" | "right";
-export type NavigationAction =
+
+export type DefaultNavigationAction =
   | { kind: "select" }
   | { kind: "move"; direction: NavigationDirection | "back" }
   | { kind: "focus"; direction: NavigationDirection | "initial" | null };
+
+export interface Register {
+  // action: DefaultNavigationAction;
+}
+
+// How to register custom action
+// declare module "@fiveway/core" {
+//   interface Register {
+//     action: DefaultNavigationAction | { kind: "custom" };
+//   }
+// }
+
+export type NavigationAction = Register extends {
+  action: infer Action;
+}
+  ? Action
+  : DefaultNavigationAction;
 
 export function handleAction(tree: NavigationTree, action: NavigationAction) {
   const targetId = runHandler(tree, tree.focusedId, action);
