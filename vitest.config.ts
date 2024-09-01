16cae906c4fd303fa8659794a5d46e77f872a031
Pavel Hrách <pavel@hrach.email>
2024-09-01 20:43:14 +0200

    ci: add coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
     coverage: {
       include: ["packages/fiveway"],
       exclude: ["**/dist/**", ...coverageConfigDefaults.exclude],
+      reporter: ["text", "json", "json-summary"],
+      reportOnFailure: true,
     },
   },
 });
