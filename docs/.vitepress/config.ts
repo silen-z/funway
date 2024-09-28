12caee5b9aefde80828848e4debf288ef4549b36
Pavel Hrách <pavel@hrach.email>
2024-09-28 12:41:29 +0200

    docs: remove extra config, clean urls & add demo link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,12 +4,16 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "fiveway",
   description: "TypeScript library for rich web applications that want to support keyboard navigation and have precise control over what is focused", 
+  cleanUrls: true,
   themeConfig: {
+    search: {
+      provider: 'local'
+    }, 
     logo: { src: '/logo-small.png', width: 24, height: 24 },
 
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Demo', link: 'https://react-demo.fiveway.io' }
     ],
 
     sidebar: [
